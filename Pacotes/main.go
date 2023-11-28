c9094d4259c44d6a8269c03bed03b4ea086d9f44
Ricardo Alencar <ricfilho0007@gmail.com>
2023-11-27 21:36:47 -0300

    Pacotes externos

--- Pacotes/main.go
+++ Pacotes/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"pacotes/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Esquevendo do arquivo main")
 	auxiliar.Escrever()
+
+	err := checkmail.ValidateFormat("ricfilho0007@gmail.com")
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("E-mail válido")
+	}
 }
